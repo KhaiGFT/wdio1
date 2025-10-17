da151db0d41a2e85e423d9d2972ccd62c3688ff6
KhaiGFT <khai.quangtran@gft.com>
2025-10-17 11:13:30 +0700

    format code

--- config/wdio.ios.app.conf.ts
+++ config/wdio.ios.app.conf.ts
@@ -18,31 +18,31 @@ export const config: WebdriverIO.Config = {
     // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
     capabilities:{
         mobile: {
-        capabilities: {
-            platformName: "iOS",
-            "wdio:maxInstances": 1,
-            // For W3C the appium capabilities need to have an extension prefix
-            // This is `appium:` for all Appium Capabilities which can be found here
-            // http://appium.io/docs/en/writing-running-appium/caps/
+            capabilities: {
+                platformName: "iOS",
+                "wdio:maxInstances": 1,
+                // For W3C the appium capabilities need to have an extension prefix
+                // This is `appium:` for all Appium Capabilities which can be found here
+                // http://appium.io/docs/en/writing-running-appium/caps/
 
-            //
-            // NOTE: Change this name according to the Simulator you have created on your local machine
-            "appium:deviceName": "iPhone 16",
-            //
-            // NOTE: Change this version according to the Simulator Version you have created on your local machine
-            "appium:platformVersion": "18.4",
-            "appium:orientation": "PORTRAIT",
-            "appium:automationName": "XCUITest",
-            // The path to the app
-            "appium:app": join(
-                process.cwd(),
-                "apps",
-                // Change this name according to the app version you downloaded
-                "ios.simulator.wdio.native.app.v1.0.8.zip"
-            ),
-            "appium:newCommandTimeout": 240,
-            // This is needed to wait for the webview context to become available
-            "appium:webviewConnectTimeout": 5000,
+                //
+                // NOTE: Change this name according to the Simulator you have created on your local machine
+                "appium:deviceName": "iPhone 16",
+                //
+                // NOTE: Change this version according to the Simulator Version you have created on your local machine
+                "appium:platformVersion": "18.4",
+                "appium:orientation": "PORTRAIT",
+                "appium:automationName": "XCUITest",
+                // The path to the app
+                "appium:app": join(
+                    process.cwd(),
+                    "apps",
+                    // Change this name according to the app version you downloaded
+                    "ios.simulator.wdio.native.app.v1.0.8.zip"
+                ),
+                "appium:newCommandTimeout": 240,
+                // This is needed to wait for the webview context to become available
+                "appium:webviewConnectTimeout": 5000,
             }
         }
     }
